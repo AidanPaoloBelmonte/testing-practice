7806697c56bedd9fe95307e0948ddf5e579cd6d3
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-09-15 18:32:56 +0800

    feat: added all tests for calculator object

--- src/scripts/calculator.test.js
+++ src/scripts/calculator.test.js
@@ -7,3 +7,31 @@ test("Integer Addition", () => {
 test("Float Addition", () => {
   expect(base.calculator.add(8.7, 9.8)).toBeCloseTo(18.5);
 });
+
+test("Integer Subtraction", () => {
+  expect(base.calculator.subtract(100, 75)).toBe(25);
+});
+
+test("Float Subtraction", () => {
+  expect(base.calculator.subtract(50.8, 25.55)).toBeCloseTo(25.25);
+});
+
+test("Integer Multiplication", () => {
+  expect(base.calculator.multiply(8, 8)).toBe(64);
+});
+
+test("Float Multiplication", () => {
+  expect(base.calculator.multiply(1.23, 4.5)).toBeCloseTo(5.535);
+});
+
+test("Integer Division", () => {
+  expect(base.calculator.divide(273, 13)).toBe(21);
+});
+
+test("Float Division", () => {
+  expect(base.calculator.divide(1000, 5.5)).toBeCloseTo(181.818181);
+});
+
+test("Zero Division", () => {
+  expect(base.calculator.divide(98765, 0)).toBe(Infinity);
+});
